11b2579454743045df51f90d2ed82918977b1b5f
islammazharul <islammazharul3645@gmail.com>
2023-05-12 12:02:21 +0600

    back comment in repo

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 6500;
 app.use(cors());
 app.use(express.json());
 
+// console.log(process.env.DB_PASS);
+// console.log(process.env.DB_USER);
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h8hwzy8.mongodb.net/?retryWrites=true&w=majority`;
