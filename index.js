2c512d64150174d8958d14939b0c8894187bf4b1
islammazharul <islammazharul3645@gmail.com>
2023-05-14 01:46:37 +0600

    added some data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -24,6 +25,25 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+const verifyJWT = (req, res, next) => {
+    // console.log('hitting verify jwt');
+    // console.log(req.headers.authorization);
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    const token = authorization.split(' ')[1]
+    console.log('token inside verify JWT', token);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -38,6 +58,7 @@ async function run() {
             res.send(result)
         })
 
+        // services routes
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -50,8 +71,15 @@ async function run() {
             // console.log(result)
         })
 
-        // bookings
-        app.get('/booking', async (req, res) => {
+        // bookings routes
+        app.get('/booking', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log(decoded);
+
+            if (decoded.email !== req.query.email) {
+                return res.status(403).send({ error: 1, message: 'forbidden access' })
+            }
+
             let query = {};
             if (req.query?.email) {
                 query: { req.query.email }
@@ -91,6 +119,16 @@ async function run() {
             const query = { _id: new ObjectId(id) };
             const result = await bookingCollection.deleteOne(query);
             res.send(result)
+        })
+
+        // jwt
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            console.log(token);
+            res.send({ token })
         })
 
         // Send a ping to confirm a successful connection
